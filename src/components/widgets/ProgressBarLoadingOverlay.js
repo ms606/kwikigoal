05af9bed4c4dfeac39eab712fbd74e51c8f6979d
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2025-05-09 16:32:04 +0500

    update

--- src/components/widgets/ProgressBarLoadingOverlay.js
+++ src/components/widgets/ProgressBarLoadingOverlay.js
@@ -30,7 +30,7 @@ const ProgressBarLoadingContainer = styled.div`
   box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.15), 0px 0px 3px 0px rgba(0, 0, 0, 0.1);
 
   @media screen and (max-width: 766px){
-   width: 90%;
+   width: 95%;
    height: auto;
   }
 `;
